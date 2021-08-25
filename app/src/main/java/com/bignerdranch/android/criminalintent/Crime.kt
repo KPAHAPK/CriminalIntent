29414dbd76f9a7a70ab064b27c48ce0ebdfb1cfa
KPAHAPK <Roman444.zaharov@gmail.com>
2021-08-25 10:33:02 +0300

    Constraintlayout features

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -4,4 +4,6 @@ class Crime(
     val id: UUID = UUID.randomUUID(),
     var title: String = "",
     var date: Date = Date(),
-    var isSolved: Boolean = false)
+    var isSolved: Boolean = false,
+    var requiresPolice: Boolean = false
+)
